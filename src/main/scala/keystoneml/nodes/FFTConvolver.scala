45d5d7e8691e13631236c161de9213a764857712
Tomer Kaftan <tomer.kaftan@gmail.com>
2017-08-24 18:45:38 -0700

    quick test change in fft

--- src/main/scala/keystoneml/nodes/FFTConvolver.scala
+++ src/main/scala/keystoneml/nodes/FFTConvolver.scala
@@ -85,6 +85,13 @@ case class FFTConvolver(filters: DenseMatrix[Double], filterSize: Int, numChanne
     res
   }
 
+  def complexPadMat(m: DenseMatrix[Complex], nrows: Int, ncols: Int): DenseMatrix[Complex] = {
+    val res = DenseMatrix.zeros[Complex](nrows, ncols)
+    res(0 until m.rows, 0 until m.cols) := m
+    res
+  }
+
+
   /**
    * Convolves an n-dimensional image with a k-dimensional
    *
@@ -103,7 +110,8 @@ case class FFTConvolver(filters: DenseMatrix[Double], filterSize: Int, numChanne
 
     val start = System.currentTimeMillis
     val fftXs = (0 until chans).map(c => fft(getChannelMatrix(x, c)))
-    val fftMs = (0 until m.length).map(f => (0 until chans).map(c => fft(padMat(getChannelMatrix(m(f), c), x.metadata.xDim, x.metadata.yDim)))).toArray
+    //val fftMs = (0 until m.length).map(f => (0 until chans).map(c => fft(padMat(getChannelMatrix(m(f), c), x.metadata.xDim, x.metadata.yDim)))).toArray
+    val fftMs = (0 until m.length).map(f => (0 until chans).map(c => (complexPadMat(fft(getChannelMatrix(m(f), c)), x.metadata.xDim, x.metadata.yDim)))).toArray
 
     //logInfo(s"Length of Xs: ${fftXs.length}, Length of each m: ${fftMs.first.length}, Total ms: ${fftMs.length}")
     var c = 0
